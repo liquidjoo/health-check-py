d413b52088e02f1d3c5e45220e4ccd8ccd13fa96
liquidjoo <liquidjoo@gmail.com>
2018-11-08 13:55:20 +0900

    config 수정 및 ElasticSearch out

--- ApiToElasticSearch.py
+++ ApiToElasticSearch.py
@@ -1,26 +1,52 @@
 import configparser
 import requests
-import io
+# pip install requests, when your code make an Error
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
 # info: Input api setting
-api_url: str = config['INPUT_API']['URL']
-api_method: str = config['INPUT_API']['METHOD']
+if "IN.API" in config:
+    api_url: str = config['IN.API']['URL']
+    api_method: str = config['IN.API']['METHOD']
 
-# info: Output setting
-hosts: str = config['OUT_PUT']['HOSTS']
-action: str = config['OUT_PUT']['ACTION']
-
-headers = {
-    'Authorization': '',
-    'access-token': ''
-}
-
+# Api Method Header setting for make custom
+# input condition
 if api_method == 'GET':
     req = requests.get(api_url)
 
+# info: Output setting for Elasticsearch
+if "OUT.ES" in config:
+    import datetime
+    hosts: str = config['OUT.ES']['HOSTS']
+    action: str = config['OUT.ES']['ACTION']
+    index = "logstash-health"+datetime.datetime.now().strftime('%Y-%m-%d')
+    doc_type: str = config['OUT.ES']['DOCUMENT_TYPE']
+    headers = {
+        # 'Authorization': '',
+        # 'access-token': '',
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+
+
+    # output condition
+    if action == 'index':
+        body: dict = req.json()
+        print(body)
+        hosts = "{}/{}/{}".format(hosts, index, doc_type)
+        response = requests.post(url=hosts, json=body, headers=headers)
+
+        print(response.json())
+        print(type(response))
+
+
+
+
+
+
+
 print(req.json())
 print(type(req.json()))
+
+
 
